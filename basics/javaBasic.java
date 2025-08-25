9e394ed379c8e78c0b4756937ab112e7d31ac01f
nishant8272 <nishantverma01042005@gmail.com>
2025-08-25 23:07:31 +0530

    basic of java

---
+++ basics/javaBasic.java
@@ -0,0 +1,63 @@
+package basics;
+import java.util.*;
+
+
+/*
+ to print the pattern 
+ *****
+ ****
+ ***
+ **
+ *
+ public class javaBasic{
+    public static void main(String []args){
+        for(int i=5;i>=0;i--){
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}  
+*/
+
+/*
+//input in java using Scanner 
+public class javaBasic{
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println("value is :"+input);
+    }
+}
+*/
+
+
+/*
+type convertion 
+it will happen when 
+1 type compatible 
+2 byte->short->int->float->long->double
+
+public class javaBasic{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        float num = sc.nextInt();
+        System.out.println(num);
+    }
+}
+*/
+
+
+ 
+// by type casting
+//we can change the big data type variable into small data type variable
+public class javaBasic{
+    public static void main(String []args){
+        int b = (int)(999.99f);
+        System.out.println(b);
+        
+    }
+}  
+
+
