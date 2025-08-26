16d87e3490323cbf2c3facd9ce82a34470bdfc8e
nishant8272 <nishantverma01042005@gmail.com>
2025-08-26 23:26:03 +0530

    simple patherns

---
+++ basics/index2.java
@@ -0,0 +1,89 @@
+package basics;
+
+import java.util.*;
+
+//if else
+
+// public class index2 {
+//     public static void main(String []args){
+//     Scanner sc = new Scanner(System.in);
+//     float salary = sc.nextFloat();
+//     double tax;
+//     if(salary <500000){
+//         tax = 0;
+//     }
+//     else if(salary >=500000 ||salary <=1000000 ){
+//         tax= (salary)*(0.3);
+//     }
+//     else{
+//         tax= (salary)*(0.4);
+//     }
+//     System.out.println("tax amount :" +tax);
+//     }
+// }
+
+
+
+//pattern printing
+
+
+// public class index2 {
+//  public static void main(String[] args) {
+//      for (int i = 0; i < 4; i++) {
+//         for (int j = 0; j <=4; j++) {
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//      }
+//  }    
+// }
+
+// public class index2 {
+//   public static void main(String[] args) {
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < i; j++) {
+//            System.out.print("*");           
+//         }
+//         System.out.println();
+//     }
+//   }
+// }
+
+// public class index2 {
+//   public static void main(String[] args) {
+//     for (int i = 4; i >0; i--) {
+//         for (int j = 0; j < i; j++) {
+//            System.out.print("*");           
+//         }
+//         System.out.println();
+//     }
+//   }
+// }
+
+
+
+// public class index2 {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 4; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class index2 {
+//   public static void main(String[] args) {
+//      Scanner sc = new Scanner(System.in);
+//      int n= sc.nextInt();
+//    for (int i = 0; i <n; i++) {
+//         for (int j = 0; j <=i; j++){
+//        System.out.print((char)(j+65));
+//         }
+//         System.out.println();
+//     }
+//   }
+// }
+
