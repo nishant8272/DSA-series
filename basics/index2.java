3a70dda25f4d857268a0d650af212b037be4a319
nishant8272 <nishantverma01042005@gmail.com>
2025-08-28 01:42:02 +0530

    functions

--- basics/index2.java
+++ basics/index2.java
@@ -22,10 +22,7 @@ import java.util.*;
 //     }
 // }
 
-
-
 //pattern printing
-
 
 // public class index2 {
 //  public static void main(String[] args) {
@@ -60,8 +57,6 @@ import java.util.*;
 //   }
 // }
 
-
-
 // public class index2 {
 //     public static void main(String[] args) {
 //         for (int i = 1; i <= 4; i++) {
@@ -72,7 +67,6 @@ import java.util.*;
 //         }
 //     }
 // }
-
 
 // public class index2 {
 //   public static void main(String[] args) {
@@ -87,3 +81,126 @@ import java.util.*;
 //   }
 // }
 
+//functions
+
+//binomial coefficient
+
+// public class index2 {
+//     public static int factorial(int n) {
+//         int ans = 1;
+//         for (int i = 1; i <= n; i++) {
+//             ans = ans * i;
+//         }
+//         return ans;
+//     }
+//     public static void Binomial(int n, int r) {
+//         int x = factorial(r);
+//         int den = factorial(n - r);
+//         int res = factorial(n) / (x * den);
+//         System.out.println(res);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter value of n : ");
+//         int n = sc.nextInt();
+//         System.out.println("enter value of r : ");
+//         int r = sc.nextInt();
+//         Binomial(n, r);
+//     }
+// }
+
+// prime number 
+
+// public class index2 {
+
+//     public static Boolean isPrime(int n) {
+//         if (n <= 1) {
+//             return false;  // 0 and 1 are not prime
+//         }
+//         if (n == 2) {
+//             return true;   // 2 is prime
+//         }
+//         if (n % 2 == 0) {
+//             return false;  // eliminate even numbers quickly
+//         }
+
+//         int sqrt = (int) Math.sqrt(n);
+//         for (int i = 3; i <= sqrt; i += 2) { // check only odd numbers
+//             if (n % i == 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     public static void isPrimeRange(int n) {
+//         for (int i = 2; i <= n; i++) {
+//             if (isPrime(i)) {
+//                 System.out.println("it is a prime number : " + i);
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+
+//         if (isPrime(n)) {
+//             System.out.println("it is a prime number.");
+//         } else {
+//             System.out.println("it is not a prime number.");
+//         }
+
+//         System.out.println("Prime numbers up to " + n + " are:");
+//         isPrimeRange(n);
+
+//         sc.close();
+//     }
+// }
+
+//Binary to decimal
+
+// public class index2 {
+//     public static void binaryToDecimal() {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int pow = 0;
+//         int dec = 0;
+//         while (n > 0) {
+//             int last = n % 10;
+//             dec = dec + (last * (int) (Math.pow(2, pow)));
+//             pow++;
+//             n = n / 10;
+//         }
+//         System.out.println("decimal value : " + dec);
+
+//     }
+
+//     public static void main(String[] args) {
+//         binaryToDecimal();
+//     }
+// }
+
+
+//decimal to binary 
+
+// public class index2{
+//     public static void binaryToDecimal() {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter decimal value : ");
+//         int n = sc.nextInt();
+//         int pow = 0;
+//         int dec = 0;
+//         while (n > 0) {
+//             int last = n % 2;
+//             dec = dec + (last * (int)(Math.pow(10, pow)));
+//             pow++;
+//             n = n / 2;
+//         }
+//         System.out.println("binay  value : " + dec);
+//     }
+//     public static void main(String[] args) {
+//         binaryToDecimal();
+//     }
+// }
